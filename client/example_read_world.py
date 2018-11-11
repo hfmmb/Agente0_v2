d642b522b28bd061c514903fb06222f0b74c8b7d
Tostaday <tostaday@hotmail.com>
2018-11-10 23:45:34 -0100

    General Fixes

--- client/example_read_world.py
+++ client/example_read_world.py
@@ -4,25 +4,26 @@ import random
 import socket
 
 class Client:
-    def __init__(self,HOST='127.0.0.1',PORT=50000):
+
+    def __init__(self,  HOST='127.0.0.1',   PORT=50000):
         self.host = HOST
         self.port = PORT
-    def print_message(self,data):
-        print("Data:",data)
+
+    def print_message(self, data):
+
+        print("Data:",  data)
+
     def connect(self):
-#        try:
+
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.connect((self.host, self.port))
-#            return(0)
-#        except:
-#            print('A connection error occurred!')
-#            return(-1)
-    def execute(self,action,value,sleep_t = 0.5):
+
+    def execute(self,   action, value,  sleep_t=0.5):
+
         self.s.sendall(str.encode(action+" "+value))
         data = self.s.recv(2048)
         print('Received', repr(data))
         msg = data.decode()
-        #message(ast.literal_eval(data.decode()))
         time.sleep(sleep_t)
         return msg
 
@@ -39,6 +40,7 @@ def getMaxCoord():
 	return max_coord   
 
 def getObstacles():
+
 	msg = c.execute("info","obstacles")
 	w_map =ast.literal_eval(msg)
 	# test
@@ -46,6 +48,7 @@ def getObstacles():
 	return w_map
 
 def main():
+
 	res = getMaxCoord()
 	print("Max coord x:",res[0])
 	print("Max coord y:",res[1])

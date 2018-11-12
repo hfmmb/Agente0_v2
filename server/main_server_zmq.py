f05e16fd8794a5abe92a9445f0be913efb12bfb7
Tostaday <tostaday@hotmail.com>
2018-11-12 22:48:37 -0100

    Documentação
    Signed-off-by: Tostaday <tostaday@hotmail.com>

--- server/main_server_zmq.py
+++ server/main_server_zmq.py
@@ -1,3 +1,17 @@
+"""
+@autores: Helder Filipe M. de M. Braga <helderbraga.work@gmail.com>;
+          João Pedro Moreira Sousa <joao.sousa201@gmail.com>;
+          Leandro Jorge O. Branco;
+
+@since: 12/11/2018
+
+Creates a client server connection to send and receive commands with the help of the module ZeroMQ <pyzmq>, this method
+makes the connection more reliable and stable than doing it directly using sockets. This is the Server side of the
+Client-Server network arquiteture.
+
+ZeroMQ: http://zguide.zeromq.org/
+"""
+
 from server_zmq import Server
 
 s = Server()

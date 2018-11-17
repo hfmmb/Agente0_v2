6ac6172c9b617a6fa4a99c44a7b933d3e8cb88e7
Tostaday <tostaday@hotmail.com>
2018-11-17 16:16:45 -0100

    Minor server alterations.
    Signed-off-by: Tostaday <tostaday@hotmail.com>

--- server/server_zmq.py
+++ server/server_zmq.py
@@ -7,7 +7,9 @@ class Server:
     def __init__(self, ip="127.0.0.1", port=50000):
         context = zmq.Context()
         socket = context.socket(zmq.REP)
-        socket.bind("tcp://" + ip + ":" + str(port))
+
+        socket.bind("tcp://*:" + str(port))
+        #socket.bind("tcp://" + ip + ":" + str(port))
         #socket.bind("tcp://wlp8s0:" + str(port))
         self.connected = socket
         pass
